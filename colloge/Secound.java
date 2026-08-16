a2208ead22d945f68de46863ed9a94d085f078b1
hanter2926 <vikrampal505@yahoo.com>
2026-08-16 20:06:22 +0530

    Add Secound class with main method to demonstrate variable usage and output

---
+++ colloge/Secound.java
@@ -0,0 +1,18 @@
+package colloge;
+
+public class Secound {
+
+    int a= 20;
+    static double b= 30.2;//static variable
+       public static void main (String[]args){
+          boolean c= true; //local variable
+
+        Secound s = new Secound();
+        System.out.println(s.a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println("vikram pal");
+
+       }
+
+}
